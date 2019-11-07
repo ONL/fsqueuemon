67de0fd412f9e9c6c3014fe49944dc9e2ab7aa7f
ONL <chinook-media@gmx.de>
2019-11-07 09:22:28 +0100

    Enhance setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
+#!/usr/bin/env python
+
 from distutils.core import setup
 setup(name='fsqueuemon',
       version='0.1.1',
+      author='Markus Lindenberg',
+      description='Call Center Queue Monitor for FreeSwitch',
+      url='https://github.com/gonicus/fsqueuemon',
       py_modules=['queuemon'],
       )
