159168b0d9f917e6e96c320bca4cea1338024bde
ONL <chinook-media@gmx.de>
2019-11-07 14:06:39 +0100

    Delete i18n.py

--- i18n.py
+++
@@ -1,6 +0,0 @@
-import gettext
-import os
-
-localedir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'locale')
-translate = gettext.translation('fsqueuemon', localedir, fallback=True)
-_ = translate.gettext
