e6bdc3fc4b15e01bcd64473f7e14324cc4fe4fe3
ONL <chinook-media@gmx.de>
2019-11-08 10:31:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@
 
 from distutils.core import setup
 from babel.messages import frontend as babel
+
 setup(name='fsqueuemon',
       version='0.1.1',
       author='Markus Lindenberg',
       description='Call Center Queue Monitor for FreeSwitch',
       url='https://github.com/gonicus/fsqueuemon',
       py_modules=['queuemon'],
+      install_requires=["flask", "flask_babel"],
+      extras_require={"test": ["pytest", "coverage"]},
       cmdclass = {'compile_catalog': babel.compile_catalog,
                 'extract_messages': babel.extract_messages,
                 'init_catalog': babel.init_catalog,
